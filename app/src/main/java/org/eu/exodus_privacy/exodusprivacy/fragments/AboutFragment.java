76bf88cf84e0edf569f0da5c7bb7ef682c1e874c
Aayush Gupta <aayushgupta219@gmail.com>
2022-02-08 22:07:43 +0530

    exodus-android-app: Add a basic about fragment
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/AboutFragment.java
@@ -0,0 +1,37 @@
+package org.eu.exodus_privacy.exodusprivacy.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import org.eu.exodus_privacy.exodusprivacy.R;
+import org.eu.exodus_privacy.exodusprivacy.databinding.FragmentAboutBinding;
+
+public class AboutFragment extends Fragment {
+
+    private FragmentAboutBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
