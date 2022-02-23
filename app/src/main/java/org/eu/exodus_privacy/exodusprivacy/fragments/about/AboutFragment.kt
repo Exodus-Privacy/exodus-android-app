083496cd00372486e4481f527d3044ebe92c277c
Aayush Gupta <aayushgupta219@gmail.com>
2022-02-23 21:00:31 +0530

    DO NOT MERGE: WIP: exodus-android-app: Rework About fragment
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/about/AboutFragment.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/about/AboutFragment.kt
@@ -1,13 +1,9 @@
 package org.eu.exodus_privacy.exodusprivacy.fragments.about
 
 import android.os.Bundle
-import android.text.SpannableString
-import android.text.TextPaint
-import android.text.method.LinkMovementMethod
-import android.text.style.URLSpan
 import android.view.View
-import android.widget.TextView
-import androidx.core.text.HtmlCompat
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.eu.exodus_privacy.exodusprivacy.R
@@ -26,35 +22,10 @@ class AboutFragment : Fragment(R.layout.fragment_about) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAboutBinding.bind(view)
-
-        binding.privacyTV.apply {
-            text = HtmlCompat.fromHtml(
-                getString(R.string.privacyPolicy, privacyPolicyURL),
-                HtmlCompat.FROM_HTML_MODE_COMPACT
-            )
-            movementMethod = LinkMovementMethod.getInstance()
-            removeURLUnderline()
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun TextView.removeURLUnderline() {
-        val spannable = SpannableString(text)
-        for (urlSpan in spannable.getSpans(0, spannable.length, URLSpan::class.java)) {
-            spannable.setSpan(
-                object : URLSpan(urlSpan.url) {
-                    override fun updateDrawState(textPaint: TextPaint) {
-                        super.updateDrawState(textPaint)
-                        textPaint.isUnderlineText = false
-                    }
-                },
-                spannable.getSpanStart(urlSpan), spannable.getSpanEnd(urlSpan), 0
-            )
-        }
-        text = spannable
     }
 }
