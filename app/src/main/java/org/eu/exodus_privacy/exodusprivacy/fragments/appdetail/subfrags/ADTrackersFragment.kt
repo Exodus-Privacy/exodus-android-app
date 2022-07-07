eb9bbb077d0c51a69a157f2c7663f0bfe6f7c479
Jean-Baptiste CHARRON <jeanbaptiste.charron@outlook.fr>
2022-07-07 22:17:58 +0200

    app: Fix on ADTrackersFragment

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/appdetail/subfrags/ADTrackersFragment.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/appdetail/subfrags/ADTrackersFragment.kt
@@ -47,7 +47,9 @@ class ADTrackersFragment : Fragment(R.layout.fragment_a_d_trackers) {
                 trackersChip.apply {
                     val trackerNum = app.exodusTrackers.size
                     text = if (app.exodusVersionCode == 0L) "?" else trackerNum.toString()
-                    setExodusColor(trackerNum)
+                    if (text != "?") {
+                        setExodusColor(trackerNum)
+                    }
                 }
                 trackersLearnTV.apply {
                     isClickable = true
