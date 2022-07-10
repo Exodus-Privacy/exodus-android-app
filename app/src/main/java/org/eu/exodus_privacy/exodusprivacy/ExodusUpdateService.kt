2f6bc69f964ab181e952e1de9f0f0c22db06b050
J-Jamet <jeremy.jamet@kunzisoft.com>
2022-07-10 17:35:32 +0200

    fix: minSDK 21 - aa0da6ce27b2b142a1e3f21fc657972dbd321285 - NotificationManagerCompat & registerNetworkCallback

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/ExodusUpdateService.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/ExodusUpdateService.kt
@@ -1,15 +1,15 @@
 package org.eu.exodus_privacy.exodusprivacy
 
 import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.lifecycleScope
@@ -71,10 +71,10 @@ class ExodusUpdateService : LifecycleService() {
     lateinit var notificationBuilder: NotificationCompat.Builder
 
     @Inject
-    lateinit var notificationManager: NotificationManager
+    lateinit var notificationManager: NotificationManagerCompat
 
     @Inject
-    lateinit var notificationChannel: NotificationChannel
+    lateinit var notificationChannel: NotificationChannelCompat
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/manager/network/NetworkManager.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/manager/network/NetworkManager.kt
@@ -3,6 +3,8 @@ package org.eu.exodus_privacy.exodusprivacy.manager.network
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -28,7 +30,10 @@ class NetworkManager @Inject constructor(
         ContextCompat.getSystemService(
             context,
             ConnectivityManager::class.java
-        )?.registerDefaultNetworkCallback(
+        )?.registerNetworkCallback(
+            NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build(),
             object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
