c446fb2c3518e4b842ae0a1c56958e5f3e22099c
Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
2022-07-19 18:06:02 +0200

    app: Fix tracker chip (#187)
    On tracker chip set default color when value is `?`

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/CommonExtensions.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/CommonExtensions.kt
@@ -9,27 +9,36 @@ import org.eu.exodus_privacy.exodusprivacy.manager.database.app.ExodusApplicatio
 import org.eu.exodus_privacy.exodusprivacy.objects.VersionReport
 
 fun Chip.setExodusColor(size: Int) {
-    val colorRed = ContextCompat.getColor(context, R.color.colorRedLight)
-    val colorYellow = ContextCompat.getColor(context, R.color.colorYellow)
-    val colorGreen = ContextCompat.getColor(context, R.color.colorGreen)
-    val colorDark = ContextCompat.getColor(context, R.color.textColorDark)
-    val colorWhite = ContextCompat.getColor(context, R.color.textColorLikeWhite)
+    if (this.text != "?") {
+        val colorRed = ContextCompat.getColor(context, R.color.colorRedLight)
+        val colorYellow = ContextCompat.getColor(context, R.color.colorYellow)
+        val colorGreen = ContextCompat.getColor(context, R.color.colorGreen)
+        val colorDark = ContextCompat.getColor(context, R.color.textColorDark)
+        val colorWhite = ContextCompat.getColor(context, R.color.textColorLikeWhite)
 
-    val textColorStateList = when (size) {
-        0 -> ColorStateList.valueOf(colorDark)
-        in 1..4 -> ColorStateList.valueOf(colorDark)
-        else -> ColorStateList.valueOf(colorWhite)
+        val textColorStateList = when (size) {
+            0 -> ColorStateList.valueOf(colorDark)
+            in 1..4 -> ColorStateList.valueOf(colorDark)
+            else -> ColorStateList.valueOf(colorWhite)
+        }
+
+        val backgroundColorStateList = when (size) {
+            0 -> ColorStateList.valueOf(colorGreen)
+            in 1..4 -> ColorStateList.valueOf(colorYellow)
+            else -> ColorStateList.valueOf(colorRed)
+        }
+
+        this.chipIconTint = textColorStateList
+        this.setTextColor(textColorStateList)
+        this.chipBackgroundColor = backgroundColorStateList
+    } else {
+        val colorText = ContextCompat.getColor(context, R.color.colorPrimary)
+        val colorBackground = ContextCompat.getColor(context, R.color.chipColor)
+
+        this.chipIconTint = ColorStateList.valueOf(colorText)
+        this.setTextColor(colorText)
+        this.chipBackgroundColor = ColorStateList.valueOf(colorBackground)
     }
-
-    val backgroundColorStateList = when (size) {
-        0 -> ColorStateList.valueOf(colorGreen)
-        in 1..4 -> ColorStateList.valueOf(colorYellow)
-        else -> ColorStateList.valueOf(colorRed)
-    }
-
-    this.chipIconTint = textColorStateList
-    this.setTextColor(textColorStateList)
-    this.chipBackgroundColor = backgroundColorStateList
 }
 
 fun Chip.setVersionReport(app: ExodusApplication) {
