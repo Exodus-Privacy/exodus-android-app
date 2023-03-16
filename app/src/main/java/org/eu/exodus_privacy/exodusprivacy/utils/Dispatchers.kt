734a98f6fefaa8a51e1af827ed65cc13ac4dd67a
erik <erik_se@posteo.de>
2023-03-16 18:12:01 +0100

    Inject dispatchers instead of hardcoding them
    This should enhance testability in the future

---
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/Dispatchers.kt
@@ -0,0 +1,40 @@
+package org.eu.exodus_privacy.exodusprivacy.utils
+
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.components.ViewComponent
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Module
+@InstallIn(ServiceComponent::class)
+object DispatcherModule {
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
