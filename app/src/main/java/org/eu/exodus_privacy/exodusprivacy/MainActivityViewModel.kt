a938ee8454496d17679a222ead25f0904389abcb
erik <erik_se@posteo.de>
2023-05-11 11:45:25 +0200

    WIP Use notification permission info in VM

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/MainActivityViewModel.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/MainActivityViewModel.kt
@@ -3,14 +3,16 @@ package org.eu.exodus_privacy.exodusprivacy
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.eu.exodus_privacy.exodusprivacy.manager.network.NetworkManager
 import org.eu.exodus_privacy.exodusprivacy.utils.DataStoreModule
 import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
-    dataStoreModule: DataStoreModule,
+    private val dataStoreModule: DataStoreModule,
 ) : ViewModel() {
 
     @Inject
@@ -18,6 +20,20 @@ class MainActivityViewModel @Inject constructor(
 
     val policyAgreement = dataStoreModule.policyAgreement.asLiveData()
     val appSetup = dataStoreModule.appSetup.asLiveData()
+    val notificationPermissionRequested = dataStoreModule.notificationPermRequested.asLiveData()
+    val notificationPermissionGranted = dataStoreModule.notificationPermGranted.asLiveData()
+
+    fun savePostNotificationPermissionRequested(status: Boolean) {
+        viewModelScope.launch {
+            dataStoreModule.saveNotificationPermRequested(status)
+        }
+    }
+
+    fun savePostNotificationPermissionGranted(status: Boolean) {
+        viewModelScope.launch {
+            dataStoreModule.saveNotificationPermGranted(status)
+        }
+    }
 
     val networkConnection: LiveData<Boolean>
         get() {
