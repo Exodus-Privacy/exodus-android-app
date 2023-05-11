859e4007a64adb8cb7b229531f9cc8d0a9d752d2
erik <erik_se@posteo.de>
2023-05-11 11:44:01 +0200

    WIP Implement logic to store notification permission info

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/DataStoreModule.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/DataStoreModule.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -22,6 +24,28 @@ class DataStoreModule @Inject constructor(
 
     private val APP_SETUP = booleanPreferencesKey("appSetup")
     val appSetup = context.dataStore.data.map { it[APP_SETUP] ?: false }
+
+    private val NOTIFICATION_PERM_REQUESTED = booleanPreferencesKey("requested")
+    val notificationPermRequested = context.dataStore.data.map {
+        it[NOTIFICATION_PERM_REQUESTED] ?: false
+    }
+
+    private val NOTIFICATION_PERM_GRANTED = booleanPreferencesKey("granted")
+    val notificationPermGranted = context.dataStore.data.map {
+        it[NOTIFICATION_PERM_GRANTED] ?: false
+    }
+
+    suspend fun saveNotificationPermRequested(status: Boolean) {
+        context.dataStore.edit {
+            it[NOTIFICATION_PERM_REQUESTED] = status
+        }
+    }
+
+    suspend fun saveNotificationPermGranted(status: Boolean) {
+        context.dataStore.edit {
+            it[NOTIFICATION_PERM_GRANTED] = status
+        }
+    }
 
     suspend fun savePolicyAgreement(status: Boolean) {
         context.dataStore.edit {
