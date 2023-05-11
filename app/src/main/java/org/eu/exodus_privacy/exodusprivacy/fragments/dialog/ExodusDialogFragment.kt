bf3a34f84663799c519b6ca82f7e3ccee45a1c82
erik <erik_se@posteo.de>
2023-05-11 12:49:59 +0200

    Use MainActivityViewModel, implement init
    DialogFragment VM is currently redundant as it would again use
    identical functionality when saving app data. Also due to init happening
    in the Fragment now, we need to access functionality from the MAVM.

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/dialog/ExodusDialogFragment.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/dialog/ExodusDialogFragment.kt
@@ -1,31 +1,41 @@
 package org.eu.exodus_privacy.exodusprivacy.fragments.dialog
 
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import org.eu.exodus_privacy.exodusprivacy.ExodusUpdateService
+import org.eu.exodus_privacy.exodusprivacy.MainActivityViewModel
 import org.eu.exodus_privacy.exodusprivacy.R
+import org.eu.exodus_privacy.exodusprivacy.manager.storage.ExodusConfig
 
 @AndroidEntryPoint
 class ExodusDialogFragment : DialogFragment() {
 
     private val TAG = ExodusDialogFragment::class.java.simpleName
-    private val exodusDialogViewModel: ExodusDialogViewModel by viewModels()
+    private val exodusDialogViewModel: MainActivityViewModel by viewModels()
+    private val permission = "android.permission.POST_NOTIFICATIONS"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        exodusDialogViewModel.policyAgreement.observe(viewLifecycleOwner) {
-            this.dismiss()
+        exodusDialogViewModel.config.observe(viewLifecycleOwner) { config ->
+            if (config["privacy_policy"]?.enable!!) this.dismiss()
         }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // Initialize the config
+        exodusDialogViewModel.config.observe(this) {}
         return MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.warning_title))
             .setMessage(
@@ -37,10 +47,50 @@ class ExodusDialogFragment : DialogFragment() {
             .setPositiveButton(getString(R.string.accept)) { _, _ ->
                 Log.d(TAG, "Permission to transmit data granted!")
                 exodusDialogViewModel.savePolicyAgreement(true)
+                if (!isNotificationPermissionGranted()) {
+                    requestPermission()
+                }
+                startInitial()
             }
             .setNegativeButton(getString(R.string.reject)) { _, _ ->
                 activity?.finish()
             }
             .create()
     }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission())
+    {
+            isGranted: Boolean ->
+        if (isGranted) {
+            exodusDialogViewModel.saveNotificationPermissionRequested(true)
+        } else {
+            exodusDialogViewModel.saveNotificationPermissionRequested(true)
+        }
+    }
+
+    private fun isNotificationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(requireContext(),permission) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        requestPermissionLauncher.launch(permission)
+    }
+
+    private fun startInitial() {
+        exodusDialogViewModel.config.observe(this) { config ->
+            if (!config["app_setup"]?.enable!! &&
+                config["privacy_policy"]?.enable!! &&
+                !ExodusUpdateService.IS_SERVICE_RUNNING) {
+                Log.d(TAG, "Populating database for the first time.")
+                val intent = Intent(requireContext(), ExodusUpdateService::class.java)
+                intent.apply {
+                    action = ExodusUpdateService.FIRST_TIME_START_SERVICE
+                    requireContext().startService(this)
+                }
+            }
+        }
+    }
+
 }
