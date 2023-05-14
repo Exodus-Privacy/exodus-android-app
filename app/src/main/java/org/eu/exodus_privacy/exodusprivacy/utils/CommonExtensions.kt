7be68d44aa2619bc34a8f2d7188b2b2e9c642fe3
Jean-Baptiste CHARRON <jeanbaptiste.charron@outlook.fr>
2023-05-14 10:31:53 +0200

    Add new function to return localized url

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/CommonExtensions.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/utils/CommonExtensions.kt
@@ -7,6 +7,7 @@ import com.google.android.material.chip.Chip
 import org.eu.exodus_privacy.exodusprivacy.R
 import org.eu.exodus_privacy.exodusprivacy.manager.database.app.ExodusApplication
 import org.eu.exodus_privacy.exodusprivacy.objects.VersionReport
+import java.util.Locale
 
 fun Chip.setExodusColor(size: Int) {
     if (this.text != "?") {
@@ -57,4 +58,8 @@ fun Chip.setVersionReport(app: ExodusApplication) {
             Toast.LENGTH_LONG
         ).show()
     }
+}
+
+fun getLanguage(): String {
+    return Locale.getDefault().language
 }
