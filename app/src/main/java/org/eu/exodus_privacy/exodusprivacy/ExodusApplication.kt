53c2468f4813604475b000c38a10029375dd8458
the-weird-aquarian <github.serenify@simplelogin.fr>
2023-08-01 21:34:15 +0530

    updated application class

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/ExodusApplication.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/ExodusApplication.kt
@@ -1,7 +1,20 @@
 package org.eu.exodus_privacy.exodusprivacy
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ExodusApplication : Application()
+class ExodusApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // TODO:
+        //  Get checked item value from preferences and set it below instead of 0
+        val themeCheckedItem = 0
+        when (themeCheckedItem) {
+            0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+    }
+}
