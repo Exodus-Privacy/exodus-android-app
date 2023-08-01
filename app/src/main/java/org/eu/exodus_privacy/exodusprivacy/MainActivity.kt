b5e005e65668460d6e7e8c74a70fe3f82a2f292c
the-weird-aquarian <github.serenify@simplelogin.fr>
2023-08-01 20:54:02 +0530

    split theme stuff to this branch

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/MainActivity.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/MainActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -29,7 +30,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        // TODO:
+        //  get blackTheme value from preferences and set it below instead of false
+        if (false) {
+            val bgColor = ResourcesCompat.getColor(resources, android.R.color.black, theme)
+            binding.fragmentCoordinator.setBackgroundColor(bgColor)
+            window.statusBarColor = bgColor
+        }
         val bottomNavigationView = binding.bottomNavView
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
@@ -40,19 +47,20 @@ class MainActivity : AppCompatActivity() {
         viewModel.networkConnection.observe(this) { connected ->
             Log.d(TAG, "Observing Network Connection.")
             if (!connected) {
-                Snackbar.make(
-                    binding.fragmentCoordinator, R.string.not_connected, Snackbar.LENGTH_LONG
-                ).setAction(R.string.settings) {
-                    try {
-                        startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
-                    } catch (ex: android.content.ActivityNotFoundException) {
+                Snackbar
+                    .make(binding.fragmentCoordinator, R.string.not_connected, Snackbar.LENGTH_LONG)
+                    .setAnchorView(binding.bottomNavView) // Snackbar will appear above bottom nav view
+                    .setAction(R.string.settings) {
                         try {
-                            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+                            startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS))
                         } catch (ex: android.content.ActivityNotFoundException) {
-                            startActivity(Intent(Settings.ACTION_SETTINGS))
+                            try {
+                                startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+                            } catch (ex: android.content.ActivityNotFoundException) {
+                                startActivity(Intent(Settings.ACTION_SETTINGS))
+                            }
                         }
-                    }
-                }.show()
+                    }.show()
             }
         }
 
--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/dialog/ThemeDialogFragment.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/dialog/ThemeDialogFragment.kt
@@ -1,27 +1,81 @@
 package org.eu.exodus_privacy.exodusprivacy.fragments.dialog
 
 import android.app.Dialog
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.checkbox.MaterialCheckBox
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.eu.exodus_privacy.exodusprivacy.R
 
 class ThemeDialogFragment : DialogFragment() {
     private val TAG = ExodusDialogFragment::class.java.simpleName
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return MaterialAlertDialogBuilder(requireContext())
-            .setTitle(getString(R.string.title_theme))
-            .setSingleChoiceItems(
-                arrayOf(
-                    getString(R.string.dark_theme),
-                    getString(R.string.light_theme),
-                    getString(R.string.system_theme)
-                ),
-                2
-            ) { _, _ ->
-                this.dismiss()
+        val items = mutableListOf(getString(R.string.light_theme), getString(R.string.dark_theme))
+        // Show "Follow system" only on SDK >= 29
+        if (Build.VERSION.SDK_INT >= 29) {
+            items.add(
+                2, getString(R.string.system_theme)
+            )
+        }
+        // If preference initial value is 0, set initialCheckedItem to 0 or 2 based on SDK
+        // TODO:
+        //  Get preferences initial value and set it below instead of 0
+        val preferenceInitialValue = 0
+        val initialCheckedItem =
+            if (preferenceInitialValue != 0) {
+                // TODO:
+                //  Get checked item value from preferences and set it below instead of 0
+                0
+            } else {
+                when {
+                    Build.VERSION.SDK_INT >= 29 -> 2
+                    else -> 0
+                }
             }
-            .create()
+        val dialogBuilder = MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.title_theme)).setSingleChoiceItems(items.toTypedArray(), initialCheckedItem) { _, newCheckedItem ->
+                // TODO:
+                //  store newCheckedItem in preferences
+                when (newCheckedItem) {
+                    0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+                dismiss()
+            }
+            .setNegativeButton(getString(R.string.cancel)) { _, _ ->
+                dismiss()
+            }
+        val checkbox = MaterialCheckBox(requireContext())
+        checkbox.text = getString(R.string.black_theme)
+        // TODO:
+        //  Get isChecked value from preferences and set it below instead of false
+        checkbox.isChecked = false
+        checkbox.isEnabled = isNightMode()
+        checkbox.setOnCheckedChangeListener { _, isChecked ->
+            // TODO:
+            //  store isChecked in preferences
+            requireActivity().recreate()
+            dismiss()
+        }
+        val checkboxLayoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        val dpToPixels = requireContext().resources.displayMetrics.density
+        checkboxLayoutParams.marginStart = (20 * dpToPixels).toInt()
+        checkbox.layoutParams = checkboxLayoutParams
+        val dialogContainer = LinearLayout(requireContext())
+        dialogContainer.orientation = LinearLayout.VERTICAL
+        dialogContainer.addView(checkbox)
+        dialogBuilder.setView(dialogContainer)
+        return dialogBuilder.create()
+    }
+    private fun isNightMode(): Boolean {
+        return requireContext().resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
     }
 }
