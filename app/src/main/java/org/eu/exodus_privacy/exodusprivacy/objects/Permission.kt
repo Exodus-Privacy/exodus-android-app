80d250a5c51bc29416c284ea51cc9b042b05e8ff
Jean-BaptisteC <jeanbaptiste.charron@outlook.fr>
2023-09-19 10:29:48 +0200

    Fix linter

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/objects/Permission.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/objects/Permission.kt
@@ -3,5 +3,5 @@ package org.eu.exodus_privacy.exodusprivacy.objects
 data class Permission(
     val shortName: String = String(),
     val longName: String = String(),
-    val label: String = String(),
+    val label: String = String()
 )
