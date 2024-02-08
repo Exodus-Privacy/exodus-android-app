5bda16b1cc04b2fa42913defe25174344cefa0f2
Konrad Pozniak <connyduck@users.noreply.github.com>
2024-02-08 17:43:00 +0100

    Improve permission recyclerview performance (#393)
    * Improve permission recyclerview performance
    
    * Update app/src/main/res/layout/fragment_a_d_sub.xml
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
    
    * Update app/src/main/res/layout/recycler_view_ad_info_item.xml
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
    
    * Update app/src/main/res/layout/fragment_a_d_sub.xml
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
    
    * Update app/src/main/res/layout/recycler_view_ad_status_item.xml
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
    
    * Update app/src/main/res/layout/recycler_view_ad_status_item.xml
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
    
    * Update app/src/main/res/layout/recycler_view_ad_info_item.xml
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jean-Baptiste <87148630+Jean-BaptisteC@users.noreply.github.com>

--- app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/appdetail/subfrags/ADPermissionsFragment.kt
+++ app/src/main/java/org/eu/exodus_privacy/exodusprivacy/fragments/appdetail/subfrags/ADPermissionsFragment.kt
@@ -5,18 +5,22 @@ import android.view.View
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import org.eu.exodus_privacy.exodusprivacy.R
-import org.eu.exodus_privacy.exodusprivacy.databinding.FragmentADPermissionsBinding
+import org.eu.exodus_privacy.exodusprivacy.databinding.FragmentADSubBinding
 import org.eu.exodus_privacy.exodusprivacy.fragments.appdetail.AppDetailViewModel
+import org.eu.exodus_privacy.exodusprivacy.fragments.appdetail.model.ADInfoItem
+import org.eu.exodus_privacy.exodusprivacy.fragments.appdetail.model.ADInfoRVAdapter
 import org.eu.exodus_privacy.exodusprivacy.fragments.appdetail.model.ADPermissionsRVAdapter
+import org.eu.exodus_privacy.exodusprivacy.fragments.appdetail.model.ADStatusRVAdapter
+import org.eu.exodus_privacy.exodusprivacy.fragments.appdetail.model.ADStatusType
 import org.eu.exodus_privacy.exodusprivacy.utils.openURL
-import org.eu.exodus_privacy.exodusprivacy.utils.setExodusColor
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ADPermissionsFragment : Fragment(R.layout.fragment_a_d_permissions) {
+class ADPermissionsFragment : Fragment(R.layout.fragment_a_d_sub) {
 
     companion object {
         private const val googleInfoPage =
@@ -28,59 +32,50 @@ class ADPermissionsFragment : Fragment(R.layout.fragment_a_d_permissions) {
     @Inject
     lateinit var customTabsIntent: CustomTabsIntent
 
-    private var _binding: FragmentADPermissionsBinding? = null
+    private var _binding: FragmentADSubBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: AppDetailViewModel by viewModels(ownerProducer = { requireParentFragment() })
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentADPermissionsBinding.bind(view)
+        _binding = FragmentADSubBinding.bind(view)
 
-        viewModel.app.observe(viewLifecycleOwner) { app ->
-            binding.apply {
-                if (app.permissions.isEmpty()) {
-                    permissionsRV.visibility = View.GONE
-                } else {
-                    val adPermissionsRVAdapter = ADPermissionsRVAdapter()
-                    permissionsRV.apply {
-                        adapter = adPermissionsRVAdapter
-                        layoutManager = object : LinearLayoutManager(view.context) {
-                            override fun canScrollVertically(): Boolean {
-                                return false
-                            }
-                        }
-                        setItemViewCacheSize(10)
-                    }
-                    adPermissionsRVAdapter.submitList(app.permissions)
-                    permissionsStatusTV.text = getString(R.string.code_permission_found)
-                }
-                permissionsChip.apply {
-                    val permsNum = app.permissions.size.toString()
-                    text = permsNum
-                    setExodusColor(permsNum)
-                }
-                permissionsLearnGoogleTV.apply {
-                    isClickable = true
-                    setOnClickListener {
+        val adPermissionsStatusRVAdapter = ADStatusRVAdapter(ADStatusType.PERMISSIONS)
+        val adPermissionsRVAdapter = ADPermissionsRVAdapter()
+        val adPermissionsInfoRVAdapter = ADInfoRVAdapter(
+            listOf(
+                ADInfoItem(
+                    text = R.string.permission_info_dangerous,
+                    onClick = {
                         openURL(
                             customTabsIntent,
                             view.context,
                             googleInfoPage,
                         )
-                    }
-                }
-                permissionsLearnExodusTV.apply {
-                    isClickable = true
-                    setOnClickListener {
+                    },
+                ),
+                ADInfoItem(
+                    text = R.string.permission_info,
+                    onClick = {
                         openURL(
                             customTabsIntent,
                             view.context,
                             permissionsInfoPage,
                         )
-                    }
-                }
-            }
+                    },
+                ),
+            ),
+        )
+
+        binding.root.apply {
+            adapter = ConcatAdapter(adPermissionsStatusRVAdapter, adPermissionsRVAdapter, adPermissionsInfoRVAdapter)
+            layoutManager = LinearLayoutManager(view.context)
+        }
+
+        viewModel.app.observe(viewLifecycleOwner) { app ->
+            adPermissionsStatusRVAdapter.setApp(app)
+            adPermissionsRVAdapter.submitList(app.permissions)
         }
     }
 
